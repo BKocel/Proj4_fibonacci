982da38d980ba6774e6fdc690e5789fe698a23f9
BKocel <bartosz.kocel@outlook.com>
2024-12-18 11:54:17 +0100

    Finished basic functionality

---
+++ main.py
@@ -0,0 +1,27 @@
+# Generating function
+def fibonacci(l):
+    print("Generowanie ciągu, proszę czekać....")
+    string = [1,1]
+    i = 2
+    while i in range(l+3):
+        # print(i)
+        # j = i-1
+        # print(string[j])
+        # j = i-2
+        # print(string[j])
+        string.append(string[i-1] + string[i-2])
+        i+= 1
+    print(*string)
+
+
+
+
+#CLI
+print("Witaj w generatorze ciągu liczb Fibonacciego")
+lenght = int(input("Jaką długość ma mieć ciąg? :"))
+
+# Unexpected value handler:
+if lenght <1:
+    print("Niepoprawna wartość, spróbój ponownie!")
+else:
+    fibonacci(lenght)
